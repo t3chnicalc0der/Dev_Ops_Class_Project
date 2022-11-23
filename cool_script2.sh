715691028e389b0485fcaa23dfe20e3047d3071b
t3chnicalc0der <zakeeakhtar12@gmail.com>
2022-11-23 16:03:21 +0000

    added new script

---
+++ cool_script2.sh
@@ -0,0 +1,11 @@
+#A script is piece of code that contains shell commands
+
+
+echo "hello world"
+
+    git clone --single-branch --branch start https://github.com/t3chnicalc0der/Dev_Ops_Class_Project.git
+    touch test.txt
+    echo "$USER"
+    pwd
+    ls -a
+    cat README.md
